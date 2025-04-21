c11ae31d9ffc5bf295f886e05f9909a4a10cfddd
Narges Rezaie <nargesr@uci.edu>
2025-04-20 23:42:26 -0700

    add workflow

---
+++ snakemake/sn_allgenes_control_train_rep1/workflow/scripts/make_topmodel.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import Topyfic
+import pandas as pd
+import scanpy as sc
+import numpy as np
+import time
+
+
+def find_best_n_topic(n_topics, names, topmodel_outputs):
+    df = pd.DataFrame(columns=['Name', 'k', 'N'])
+    n = len(names)
+
+    for i in range(n):
+        for n_topic in n_topics:
+            name = f"{topmodel_outputs[i]}/topModel_{names[i]}_{n_topic}.p"
+            top_model = Topyfic.read_topModel(name)
+
+            tmp = pd.DataFrame([[names[i], n_topic, top_model.N]], columns=['Name', 'k', 'N'])
+            df = pd.concat([df, tmp])
+
+    res = dict()
+    for i in range(n):
+        tmp = df[df.Name == names[i]]
+        tmp['diff'] = tmp['N'] - tmp['K']
+        tmp['diff'].idxmin()
+        res[names[i]] = tmp.loc[tmp['diff'].idxmin(), 'K']
+    return df, res
+
+
+def make_top_model(trains, adata_paths, n_top_genes=50, resolution=1, max_iter_harmony=10, min_cell_participation=None,
+                   topmodel_output=None):
+    # Start the timer
+    start_time = time.time()
+    
+    # Check if the file exists
+    if not os.path.exists(topmodel_output):
+        os.makedirs(topmodel_output, mode=0o777)
+
+    sc.settings.figdir = f"{topmodel_output}/figures/"
+
+    adata = None
+    if isinstance(adata_paths, str):
+        adata = sc.read_h5ad(adata_paths)
+    else:
+        for adata_path in adata_paths:
+            tmp = sc.read_h5ad(adata_path)
+            if adata is None:
+                adata = sc.read_h5ad(adata_path)
+            else:
+                adata = adata.concatenate(tmp)
+
+    top_model, clustering, adata_topmodel = Topyfic.calculate_leiden_clustering(trains=trains,
+                                                                                data=adata,
+                                                                                n_top_genes=n_top_genes,
+                                                                                resolution=resolution,
+                                                                                max_iter_harmony=max_iter_harmony,
+                                                                                min_cell_participation=min_cell_participation)
+
+    print(top_model.name, topmodel_output)
+    top_model.save_topModel(save_path=topmodel_output)
+    print(f"{topmodel_output}topModel_{top_model.name}.p")
+
+    adata_topmodel.write_h5ad(f"{topmodel_output}/topic_weight_umap.h5ad")
+    clustering.to_csv(f"{topmodel_output}/topic_cluster_mapping.csv")
+
+    # End the timer
+    end_time = time.time()
+
+    # Calculate the elapsed time
+    elapsed_time = end_time - start_time
+    print(f"Run time: {elapsed_time:.4f} seconds")
+
+
+def make_analysis(adata_paths, top_model, topmodel_output=None):
+    
+    # Start the timer
+    start_time = time.time()
+    
+    # Check if the file exists
+    if not os.path.exists(topmodel_output):
+        os.makedirs(topmodel_output, mode=0o777)
+
+    adata = None
+    if isinstance(adata_paths, str):
+        adata = sc.read_h5ad(adata_paths)
+    else:
+        for adata_path in adata_paths:
+            tmp = sc.read_h5ad(adata_path)
+            if adata is None:
+                adata = sc.read_h5ad(adata_path)
+            else:
+                adata = adata.concatenate(tmp)
+
+    analysis_top_model = Topyfic.Analysis(Top_model=top_model)
+    analysis_top_model.calculate_cell_participation(data=adata)
+    analysis_top_model.save_analysis(save_path=topmodel_output)
+
+    # End the timer
+    end_time = time.time()
+
+    # Calculate the elapsed time
+    elapsed_time = end_time - start_time
+    print(f"Run time: {elapsed_time:.4f} seconds")
